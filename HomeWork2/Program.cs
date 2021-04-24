586f9e42b43947667ab8388422b7221ffcd70d76
PaRus <PaRusakovich@gmail.com>
2021-04-24 14:19:39 +0300

    Добавьте файлы проекта.

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if  ( a % 2 == 0  ) 
+            {
+                Console.WriteLine("Число кратно 2");
+            }
+            else 
+            {
+                Console.WriteLine("Число не кратно 2");
+            }
+        }
+    }
+}
